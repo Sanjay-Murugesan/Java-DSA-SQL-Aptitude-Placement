5519fcdbf1833160ba80100a862c6a0bbae254a6
Sanjay <148308584+Sanjay-Murugesan@users.noreply.github.com>
2026-05-23 23:53:53 +0530

    Update CountUpperLower.java

--- DSA/String/01-Basic-String-Traversal/CountUpperLower.java
+++ DSA/String/01-Basic-String-Traversal/CountUpperLower.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
 
+public class CountUpperLower {
+
+    public static void main(String[] args) {
+
+        // Creating Scanner object for input
+        Scanner sc = new Scanner(System.in);
+
+        // Getting string input from user
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        // Variables to store uppercase and lowercase count
+        int upperCount = 0;
+        int lowerCount = 0;
+
+        // Traversing each character in the string
+        for (int i = 0; i < str.length(); i++) {
+
+            // Getting character one by one
+            char ch = str.charAt(i);
+
+            // Checking if character is uppercase
+            if (Character.isUpperCase(ch)) {
+                upperCount++;
+            }
+
+            // Checking if character is lowercase
+            else if (Character.isLowerCase(ch)) {
+                lowerCount++;
+            }
+        }
+
+        // Printing the result
+        System.out.println("Uppercase letters: " + upperCount);
+        System.out.println("Lowercase letters: " + lowerCount);
+
+        // Closing scanner
+        sc.close();
+    }
+}
